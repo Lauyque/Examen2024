0b4ff62bcb7e17802785045e5caa2b46f3b0f85a
Overlord <loic.ledoher@gmail.com>
2024-06-05 16:09:48 +0200

    Exo3

---
+++ PF/Exo3/Exo3.js
@@ -0,0 +1,15 @@
+const longestWord = (mot) => {
+    const array = mot.split(" ");
+    console.log(array)
+    const initialValue = "";
+    const sumWithInitial = array.reduce(
+        (accumulator, currentValue) => {if (accumulator.length <= currentValue.length) { return currentValue } else { return accumulator }},
+            initialValue,
+    );
+    return sumWithInitial
+}
+
+
+console.log(longestWord('a b c d e fgh'))
+console.log(longestWord('one two three'))
+console.log(longestWord('red blue grey'))
